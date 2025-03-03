96221a3c062cf52c7c5d6237b517f2192d5639c6
Oliver-Atondo <oa0323@dexcom.com>
2025-03-02 17:36:43 -0800

    Initial Commit

---
+++ Practica1_PokemonDetail_OliverAtondo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1_PokemonDetail_OliverAtondo
+//
+//  Created by Oliver Atondo on 02/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
